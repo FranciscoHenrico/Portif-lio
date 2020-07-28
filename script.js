e010bdf8738e919a049d14cb066a4caaf908c9b0
unknown <f.henrico.l@gmail.com>
2020-07-28 17:53:33 -0300

    :package: js sessão orçamento

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+document.querySelector("#quantidade").addEventListener("change", atualizarPreco)
+document.querySelector("#Decorador-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#Decorador-não").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+function atualizarPreco(){
+    const quantidade = document.querySelector("#quantidade").value
+    const Decorador = document.querySelector("#Decorador-sim").checked
+    const prazo = document.querySelector("#prazo").value 
+    
+    let preco = quantidade * 100;
+    if (Decorador) preco *= 1.1
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
